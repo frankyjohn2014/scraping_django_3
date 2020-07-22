273fae023b260326a1318b96142f588577e5b2dc
frankyjohn2014 <frankyjohn2014@gmail.com>
2020-07-22 15:48:30 +0300

    add filter

--- scraping/views.py
+++ scraping/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from .models import Vacancy
 
 def home_view(request):
-    qs = Vacancy.objects.all()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
 
+        qs = Vacancy.objects.filter(**_filter)
     return render(request,'scraping/home.html', {'object_list':qs})
