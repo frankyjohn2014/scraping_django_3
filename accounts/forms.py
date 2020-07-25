7054e315ec493e9c97ceb6c11afac10d412074ab
frankyjohn <frankyjohn2014@gmail.com>
2020-07-25 23:39:50 +0300

    messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model,authenticate
 from django.contrib.auth.hashers import check_password
-
+from scraping.models import City,Language
 User = get_user_model()
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -21,3 +21,34 @@ class UserLoginForm(forms.Form):
             if not user:
                 raise forms.ValidationError('Данный аккаунт отключен')
         return super(UserLoginForm, self).clean(*args,**kwargs)
+
+class UserRegistrationForm(forms.ModelForm):
+    email = forms.CharField(label='Введите email',
+    widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='Введите пароль',
+    widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Введите пароль ещё раз',
+    widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return data['password2']
+
+class UserUpdateForm(forms.Form):
+
+    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name='slug', 
+    required=True, widget=forms.Select(attrs={'class':'form-control'}), label='Город')
+
+    language = forms.ModelChoiceField(queryset=Language.objects.all(), to_field_name='slug',
+    required=True, widget=forms.Select(attrs={'class':'form-control'}), label='Специальность')
+
+    send_email = forms.BooleanField(required=False, widget=forms.CheckboxInput,label='Получать рассылку?')
+
+    class Meta:
+        model = User
+        fields = ('city','language','send_email')
